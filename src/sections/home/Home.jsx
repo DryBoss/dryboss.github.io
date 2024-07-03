ec9c6f571fb41a8eb22657b280a72f71aa21aa16
Mohammad Taiham <mohammadtaiham.dryboss@gmail.com>
2024-07-04 02:20:15 +0600

    idk

--- src/sections/home/Home.jsx
+++ src/sections/home/Home.jsx
@@ -8,44 +8,63 @@ import githubLogo from "./../../assets/icons/github.svg";
 
 export default function Home() {
   return (
-    <div className="section">
-      <div className="intro">
-        <div className={styles.title}>
-          <img src={dryBossLogo} alt="DryBoss Logo" className={styles.logo} />
-          <h1>
-            <span>Mohammad Taiham</span>
-            <br />
-            Web developer
-          </h1>
-        </div>
-        <div className={styles.menu}>
-          <p>projects</p>
-          <p>writings</p>
-          <p>achievements</p>
-        </div>
-        <div className={styles.aboutMe}>
-          <p>
-            I am an undergraduate Mathematics student at the University of
-            Chittagong, passionate about leveraging technology and data.
-            <br />I specialize in frontend development with JavaScript and
-            React.js and am expanding into data analysis and backend
-            development. My research focuses on AI and Big Data.
-            <br />I actively contribute to various projects and enjoy gaming to
-            enhance creativity and strategic thinking
-          </p>
-          <div className={styles.moreAboutMe}>
-            <button>More about me</button>
-            <button>Download CV</button>
+    <>
+      <div className="section" id="home">
+        <div className={styles.intro}>
+          <div className={styles.title}>
+            <img src={dryBossLogo} alt="DryBoss Logo" className={styles.logo} />
+            <h1>
+              <span>Mohammad Taiham</span>
+              <br />
+              Web developer
+            </h1>
           </div>
-          <div className={styles.socials}>
-            <img src={facebookLogo} alt="facebook" />
-            <img src={instagramLogo} alt="instagram" />
-            <img src={xLogo} alt="x" />
-            <img src={linkedinLogo} alt="linkedin" />
-            <img src={githubLogo} alt="github" />
+          <div className={styles.menu}>
+            <a href="#projects">projects</a>
+            <a href="">writings</a>
+            <a href="">achievements</a>
+          </div>
+          <div className={styles.aboutMe}>
+            <p>
+              I am an undergraduate Mathematics student at the University of
+              Chittagong, passionate about leveraging technology and data.
+              <br /> <br />I specialize in frontend development with JavaScript
+              and React.js and am expanding into data analysis and backend
+              development. My research focuses on AI and Big Data.
+              <br /> <br />I actively contribute to various projects and enjoy
+              gaming to enhance creativity and strategic thinking
+              <br />
+            </p>
+            <div className={styles.moreAboutMe}>
+              <button>More about me</button>
+              <button>Download CV</button>
+            </div>
+            <div className={styles.socials}>
+              <img src={facebookLogo} alt="facebook" />
+              <img src={instagramLogo} alt="instagram" />
+              <img src={xLogo} alt="x" />
+              <img src={linkedinLogo} alt="linkedin" />
+              <img src={githubLogo} alt="github" />
+            </div>
+          </div>
+        </div>
+        <div className={styles.featured}>
+          <div className="latest-blog">
+            <h3>latest blog post</h3>
+            <p>Why you are stupid?</p>
           </div>
         </div>
       </div>
-    </div>
+      <div className="section" id="projects">
+        <div className="projects">
+          <h2>Projects</h2>
+          <div className="project-list">
+            <ul>
+              <li></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
