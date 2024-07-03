0b2b4091bb580f14ca2f026c7718c2f02c93f610
Mohammad Taiham <mohammadtaiham.dryboss@gmail.com>
2024-07-03 23:40:11 +0600

    home section complete

---
+++ src/sections/projects/Projects.jsx
@@ -0,0 +1,16 @@
+export default function Projects() {
+  return (
+    <>
+      <div className="section">
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Reprehenderit possimus nostrum, quia tempore
+          <br />
+          ea non amet nemo deleniti nesciunt. Similique laudantium
+          necessitatibus distinctio eligendi fuga, illo praesentium quos autem
+          ab.
+        </p>
+      </div>
+    </>
+  );
+}
