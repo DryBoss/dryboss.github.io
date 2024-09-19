6903f56e1eea858cd4bcadbc1b15dada850489a7
Mohammad Taiham <mohammadtaiham.dryboss@gmail.com>
2024-09-19 20:20:24 +0600

    about me

--- src/pages/home/sections/about-me/AboutMe.jsx
+++ src/pages/home/sections/about-me/AboutMe.jsx
@@ -3,22 +3,27 @@ import educationLogo from "./../../../../assets/icons/education.svg";
 
 function AboutMe() {
   return (
-    <div className={`${styles.section} home-section`}>
-      <h1>
-        Hi, I am <span>Mohammad Taiham</span>
-        <br />
-        aka <span>DryBoss</span>
-      </h1>
-      <div className={styles.subSections}>
-        <div>
-          <div className={styles.detailWithIcon}>
-            <img src={educationLogo} alt="education" /> Graduating in
-            Mathematics from University of Chittagong
-          </div>
+    <div className="home-section" id="more-about-me">
+      <div className={styles.coreSection}>
+        <h1>
+          Hi, I am <span>Mohammad Taiham</span>
+          <br />
+          aka <span>DryBoss</span>
+        </h1>
+        <div className={styles.detailWithIcon}>
+          <img src={educationLogo} alt="education" />
+          <b style={{ margin: "0 8px 0 0" }}>
+            Founder & Content Writer
+          </b> at <b style={{ margin: "0 8px" }}>The Math Project</b>
         </div>
-        <div>
-          <h3>Skills:</h3>
+        <div className={styles.detailWithIcon}>
+          <img src={educationLogo} alt="education" /> Graduating in{" "}
+          <b style={{ margin: "0 8px" }}>Mathematics</b> at{" "}
+          <b style={{ margin: "0 8px" }}>University of Chittagong</b>
         </div>
+      </div>
+      <div>
+        <h3>Skills:</h3>
       </div>
     </div>
   );
