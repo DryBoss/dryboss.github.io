35aefa6018789d6b7f1d4eff8cae2a2c58e46e8e
Mohammad Taiham <mohammadtaiham.dryboss@gmail.com>
2025-02-28 23:58:46 +0600

    idk

--- src/database/data.js
+++ src/database/data.js
@@ -9,6 +9,7 @@ export const myself = {
     "I'm a passionate tech enthusiast with a solid foundation in data science, web development, quantum computing, and cryptography. Driven by curiosity and creativity, I strive to push boundaries and explore new possibilities in the tech world.",
     "Feel free to explore my work, and don't hesitate to reach out if you're interested in collaborating!",
   ],
+  skills: ["JS", "React JS"],
   thingsILike: [
     "gaming",
     "coding",
