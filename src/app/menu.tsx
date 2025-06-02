e5e85eb8c8d1b88d49eb9a54f1514fa43931b7cf
Mohammad Taiham <mohammadtaiham.dryboss@gmail.com>
2025-06-02 18:21:51 +0600

    home

--- src/app/menu.tsx
+++ src/app/menu.tsx
@@ -2,23 +2,34 @@ import Image from "next/image";
 
 export default function Menu() {
   const menuItems = [
-    { title: "projects", icon: "./icons/rocket.svg", redirect: "" },
-    { title: "achievements", icon: "./icons/achievements.svg", redirect: "" },
+    {
+      title: "projects",
+      icon: "./icons/rocket.svg",
+      redirect: "",
+      rotation: "-5",
+    },
+    {
+      title: "achievements",
+      icon: "./icons/achievements.svg",
+      redirect: "",
+      rotation: "5",
+    },
   ];
 
   return (
-    <div className="flex">
-      {menuItems.map(({ title, icon, redirect }, index) => (
+    <div className="flex fixed bottom-[-24] left-1/2 -translate-x-1/2">
+      {menuItems.map(({ title, icon, redirect, rotation }, index) => (
         <div
           key={title}
-          className="w-32 h-40 m-4 p-2 bg-secondary-light text-xs tracking-widest"
+          className={`w-32 h-40 m-0 p-3 bg-secondary-light text-xs tracking-widest flex flex-col justify-between items-center rounded-2xl border-4 border-primary-light hover:-translate-y-2 transition-transform duration-200 cursor-pointer`}
+          style={{ transform: `rotate(${rotation}deg)` }}
         >
           <p>{title}</p>
           <Image
             src={icon}
             alt={`${title}`}
-            width={64}
-            height={64}
+            width={56}
+            height={56}
             priority={true}
           />
           <p>{title}</p>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,26 @@ import Menu from "./menu";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="h-screen flex justify-center items-center">
       <div className="">
         <Menu />
+        <div className="p-4 m-8 bg-secondary-dark text-gray-300 rounded-lg">
+          <h1 className="m-2 text-4xl">Hi, I’m Taiham</h1>
+          <p className="m-4 text-lg">
+            full stack developer | machine learning engineer | quantum computing
+            researcher
+          </p>
+          <p className="m-2">
+            a curious mind passionate about technology, learning, and building
+            impactful solutions. Whether it’s developing intuitive web
+            experiences, exploring the depths of machine learning, or diving
+            into mathematical concepts, I’m always eager to push boundaries and
+            create something meaningful. This site is a reflection of my journey
+            — projects, ideas, experiments, and the things I’m learning along
+            the way.<br></br>
+            Feel free to explore, reach out, or just say hi!
+          </p>
+        </div>
       </div>
     </div>
   );
