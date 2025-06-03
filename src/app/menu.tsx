87236709cf241531a3e0a0b632982b8c237e3b99
Mohammad Taiham <mohammadtaiham.dryboss@gmail.com>
2025-06-03 22:31:57 +0600

    menu

--- src/app/menu.tsx
+++ src/app/menu.tsx
@@ -1,3 +1,6 @@
+"use client";
+import { useEffect, useState } from "react";
+
 export default function Menu() {
   const menuItems = [
     {
@@ -10,14 +13,33 @@ export default function Menu() {
     },
   ];
 
+  const [visibleItems, setVisibleItems] = useState<number[]>([]);
+
+  useEffect(() => {
+    const timeouts: NodeJS.Timeout[] = [];
+
+    menuItems.forEach((_, index) => {
+      const timeout = setTimeout(() => {
+        setVisibleItems((prev) => [...prev, index]);
+      }, 300 + 200 * index);
+      timeouts.push(timeout);
+    });
+
+    return () => {
+      timeouts.forEach(clearTimeout);
+    };
+  }, []);
+
   return (
-    <div className="flex fixed bottom-[-24] left-1/2 -translate-x-1/2 z-99">
+    <div className={`flex fixed bottom-[-24] left-1/2 -translate-x-1/2 z-99`}>
       {menuItems.map(({ title, rotation }, index) => (
         <div
           key={title}
           className={`w-32 h-40 m-0 p-3 bg-card-white text-xs ${
             index % 2 ? "text-card-red" : "text-card-black"
-          } tracking-widest flex flex-col justify-between items-center rounded drop-shadow-lg hover:-translate-y-2 transition-transform duration-200 cursor-pointer`}
+          } tracking-widest flex flex-col justify-between items-center rounded drop-shadow-lg hover:-translate-y-2 duration-200 cursor-pointer transition-all transform ${
+            visibleItems.includes(index) ? "translate-y-0" : "translate-y-500"
+          } ease-out`}
           style={{ transform: `rotate(${rotation}deg)` }}
         >
           <p>{title}</p>
