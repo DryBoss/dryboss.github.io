c02c4c08e1eddbd1e0c499bf7323670e4c5d8a84
Mohammad Taiham <mohammadtaiham.dryboss@gmail.com>
2025-06-04 11:06:40 +0600

    new menu

--- src/app/menu.tsx
+++ src/app/menu.tsx
@@ -6,10 +6,17 @@ export default function Menu() {
     {
       title: "projects",
       rotation: "-5",
+      translateY: "0",
+    },
+    {
+      title: "writings",
+      rotation: "0",
+      translateY: "-5",
     },
     {
       title: "achievements",
       rotation: "5",
+      translateY: "0",
     },
   ];
 
@@ -32,15 +39,17 @@ export default function Menu() {
 
   return (
     <div className={`flex fixed bottom-[-24] left-1/2 -translate-x-1/2 z-99`}>
-      {menuItems.map(({ title, rotation }, index) => (
+      {menuItems.map(({ title, rotation, translateY }, index) => (
         <div
           key={title}
-          className={`w-32 h-40 m-0 p-3 bg-card-white text-xs ${
+          className={`w-32 h-40 mx-[-10] p-3 bg-card-white text-xs ${
             index % 2 ? "text-card-red" : "text-card-black"
           } tracking-widest flex flex-col justify-between items-center rounded drop-shadow-lg hover:-translate-y-2 duration-200 cursor-pointer transition-all transform ${
             visibleItems.includes(index) ? "translate-y-0" : "translate-y-500"
           } ease-out`}
-          style={{ transform: `rotate(${rotation}deg)` }}
+          style={{
+            transform: `rotate(${rotation}deg) translateY(${translateY}px`,
+          }}
         >
           <p>{title}</p>
           {title == "projects" ? (
@@ -56,6 +65,15 @@ export default function Menu() {
                 clipRule="evenodd"
               />
               <path d="M5.26 17.242a.75.75 0 1 0-.897-1.203 5.243 5.243 0 0 0-2.05 5.022.75.75 0 0 0 .625.627 5.243 5.243 0 0 0 5.022-2.051.75.75 0 1 0-1.202-.897 3.744 3.744 0 0 1-3.008 1.51c0-1.23.592-2.323 1.51-3.008Z" />
+            </svg>
+          ) : title == "writings" ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-16"
+            >
+              <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32L19.513 8.2Z" />
             </svg>
           ) : (
             <svg
