8698426346aef69586ef6cbd56f1840484c50caf
Mohammad Taiham <mohammadtaiham.dryboss@gmail.com>
2025-06-27 23:19:51 +0600

    projects

--- src/app/(menu-groups)/projects/projects-component/projects.tsx
+++ src/app/(menu-groups)/projects/projects-component/projects.tsx
@@ -27,15 +27,34 @@ export default function Projects() {
   if (projects.length === 0) return <p>Loading projects...</p>;
 
   return (
-    <div>
-      {projects.map((project) => (
-        <div key={project._id}>
-          <h3>{project.name}</h3>
-          <p>{project.description}</p>
-          <p>Technologies: {project.technologies.join(", ")}</p>
-          <a href={project.link} target="_blank" rel="noopener noreferrer">
-            Project Link
-          </a>
+    <div className="mx-10 my-5">
+      {projects.map((project, index) => (
+        <div
+          key={project._id}
+          className={`flex ${index % 2 ? "flex-row-reverse text-right" : ""}`}
+        >
+          <div
+            className={`w-32 h-40 p-3 bg-card-white text-xs ${
+              index % 2 ? "text-card-red" : "text-card-black"
+            } tracking-widest flex flex-col justify-between items-center rounded drop-shadow-lg hover:scale-105 cursor-pointer transition-transform duration-200 ease-out`}
+          >
+            <p>{project.name}</p>
+            <p className="rotate-180">{project.name}</p>
+          </div>
+          <div className="p-5 flex flex-col justify-between">
+            <h3 className="text-lg">{project.name}</h3>
+            <p className="opacity-75 text-sm">{project.description}</p>
+            <div className={`flex ${index % 2 ? "justify-end" : ""}`}>
+              {project.technologies.map((technology) => (
+                <p
+                  key={technology}
+                  className="px-2 py-1 text-xs border-2 rounded-full"
+                >
+                  {technology}
+                </p>
+              ))}
+            </div>
+          </div>
         </div>
       ))}
     </div>
