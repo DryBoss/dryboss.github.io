709cc3872e437316f1c54f3c108b2cf1579b77b8
Mohammad Taiham <mohammadtaiham.dryboss@gmail.com>
2025-07-07 22:54:23 +0600

    loading

--- src/app/(menu-groups)/projects/_projectsComponent/projects.tsx
+++ src/app/(menu-groups)/projects/_projectsComponent/projects.tsx
@@ -73,7 +73,25 @@ export default function Projects({ currentCategory }: ProjectsProps) {
     });
   }, [filteredProjects]);
 
-  if (!projects.length) return <p>Loading projects...</p>;
+  if (!projects.length)
+    return (
+      <div className="flex justify-center items-center">
+        <div
+          className={`w-36 h-45 p-3 my-20 bg-card-white flex justify-center items-center rounded drop-shadow-lg transition-transform duration-200 ease-out animate-flip`}
+        >
+          <svg
+            fill="#000000"
+            width="800px"
+            height="800px"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-25 h-25"
+          >
+            <path d="M6.108,20H4a1,1,0,0,0,0,2H20a1,1,0,0,0,0-2H17.892c-.247-2.774-1.071-7.61-3.826-9,2.564-1.423,3.453-4.81,3.764-7H20a1,1,0,0,0,0-2H4A1,1,0,0,0,4,4H6.17c.311,2.19,1.2,5.577,3.764,7C7.179,12.39,6.355,17.226,6.108,20ZM9,16.6c0-1.2,3-3.6,3-3.6s3,2.4,3,3.6V20H9Z" />
+          </svg>
+        </div>
+      </div>
+    );
 
   return (
     <div className="pb-5 grid md:grid-cols-2 items-center">
