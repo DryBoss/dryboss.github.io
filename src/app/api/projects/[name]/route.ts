a146b8d3548ecfeda1b1463121c19e75b4205447
Mohammad Taiham <mohammadtaiham.dryboss@gmail.com>
2025-07-10 23:39:28 +0600

    fix

--- src/app/api/projects/[name]/route.ts
+++ src/app/api/projects/[name]/route.ts
@@ -1,18 +1,17 @@
 import { connectToDatabase } from "@/lib/db-connect";
 import { ProjectDetails } from "@/lib/models/project-details";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-interface Params {
-  params: {
-    name: string;
-  };
-}
-
-export async function GET(request: Request, { params }: Params) {
+export async function GET(
+  req: NextRequest,
+  context: { params: { name: string } }
+) {
   await connectToDatabase();
 
+  const name = decodeURIComponent(context.params.name);
+
   const projectDetail = await ProjectDetails.findOne({
-    name: decodeURIComponent(params.name),
+    name: name,
   }).lean();
 
   if (!projectDetail) {
